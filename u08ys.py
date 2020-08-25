890317822f7041e3e159ea18fc67340ed466b3bb
secvbulrep <bfarukshin@gmail.com>
2020-08-25 10:59:11 +0300

    * fix

--- u08ys.py
+++ u08ys.py
@@ -1,2 +1,17 @@
 import pandas
 data = pandas.read_csv('titanic.csv', index_col='PassengerId')
+
+#print(data[:10])
+#print(data.head())
+#print(data['Pclass'])
+
+#print(data['Survived'].value_counts())
+
+#print(100*data['Survived'].value_counts()/data['Survived'].count())
+
+#print(100*data['Pclass'].value_counts()/data['Pclass'].count())
+
+
+print(data['Age'].mean())
+
+print(data['Age'].median())
