658406972bced7bbecadb3cf98b3cb6dc3762ee3
secvbulrep <bfarukshin@gmail.com>
2020-09-01 14:50:42 +0300

    * lesson2

---
+++ statement-importance.py
@@ -0,0 +1,28 @@
+import pandas
+import re
+import sys 
+sys.path.append("..")
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import matplotlib.pyplot as plt
+import graphviz 
+
+data = pandas.read_csv('titanic.csv', index_col='PassengerId')
+
+main_data_frame = pandas.DataFrame(data=data, columns=['Pclass', 'Fare', 'Age', 'Sex', 'Survived'])
+main_data_frame = main_data_frame[["Pclass", "Fare", "Age", "Sex", 'Survived']].dropna().replace("female",0).replace("male",1)
+
+X = main_data_frame[["Pclass", "Fare", "Age", "Sex"]]
+Y = main_data_frame[["Survived"]]
+
+clf = DecisionTreeClassifier(random_state=241)
+clf.fit(X, Y)
+
+print(clf.feature_importances_)
+tree.plot_tree(clf) 
+
+
+
+
+
